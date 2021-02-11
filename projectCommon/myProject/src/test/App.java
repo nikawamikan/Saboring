d3ce7529da784dc13d1645539806b8877a63c6f0
wkwktktkkrkr@outlook.jp <66500373+nikawamikan@users.noreply.github.com>
2021-02-11 20:58:01 +0900

    test

--- projectCommon/myProject/src/test/App.java
+++ projectCommon/myProject/src/test/App.java
@@ -1,7 +1,14 @@
 package test;
+
+import projectCommon.ConsoleReaderNext;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        ConsoleReaderNext mikan = new ConsoleReaderNext();
+        mikan.getString("入力テストです。");
+
+        System.out.println(mikan);
     }
 }
-
